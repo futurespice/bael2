8b773cd312d6e251e19b8b22a3ace3f31a3a4635
futurespice <azatss442@gmail.com>
2026-03-12 23:33:03 +0600

    исправление размера страницы на 50 для мага

--- stores/views.py
+++ stores/views.py
@@ -107,7 +107,7 @@ from orders.models import (
 
 class StandardPagination(PageNumberPagination):
     """Стандартная пагинация."""
-    page_size = 20
+    page_size = 50
     page_size_query_param = 'page_size'
     max_page_size = 100
 
